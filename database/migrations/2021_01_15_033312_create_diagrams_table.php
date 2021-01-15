75c03c8725122a4eb867e310e605b9b665adaddf
W-Nakasone <psyqhical@icloud.com>
2021-01-15 12:40:39 +0900

    DB接続

--- database/migrations/2021_01_15_033312_create_diagrams_table.php
+++ database/migrations/2021_01_15_033312_create_diagrams_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateArticlesTable extends Migration
+class CreateDiagramsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,13 @@ class CreateArticlesTable extends Migration
      */
     public function up()
     {
-        Schema::create('articles', function (Blueprint $table) {
+        Schema::create('diagrams', function (Blueprint $table) {
             $table->id();
-            $table->string('content');
+            $table->string('name');
+            $table->string('departure');
+            $table->string('departure_time');
+            $table->string('arrival');
+            $table->string('arrival_time');
             $table->timestamps();
         });
     }
@@ -27,6 +31,6 @@ class CreateArticlesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('articles');
+        Schema::dropIfExists('diagrams');
     }
 }
